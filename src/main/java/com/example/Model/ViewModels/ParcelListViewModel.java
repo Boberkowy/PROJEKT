eaffcfaa0da783c4bbfc4dbef17a26293080529b
Boberkowy <bobermen@gmail.com>
2017-01-26 00:57:57 +0100

    nawigacja ze wzgledu na role

---
+++ src/main/java/com/example/Model/ViewModels/ParcelListViewModel.java
@@ -0,0 +1,13 @@
+package com.example.Model.ViewModels;
+
+/**
+ * Created by Boberkowy on 26.01.2017.
+ */
+public class ParcelListViewModel {
+  private String city;
+  private String houseNumber;
+  private String region;
+  private String street;
+  private String zipcode;
+
+}
