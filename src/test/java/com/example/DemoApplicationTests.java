dfbe2c1ad6ee53ea8385130030ec3aa1a1c8a569
Karola <s12592@pjwstk.edu.pl>
2017-01-26 00:59:10 +0100

    xx

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.Controller.HomeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +22,11 @@ import org.springframework.test.web.servlet.MockMvc;
 public class DemoApplicationTests {
 
 	@Autowired
-	private MockMvc mockMvc;
+	private HomeController homeController;
 
 	@Test
 	public void shouldReturnIndex() throws Exception{
-		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Dupa")));
+		assertThat(homeController).isNotNull();
 	}
 
 }
---
+++ src/test/java/com/example/ExampleTests.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import com.example.Controller.ProfileController;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class ExampleTests {
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new ProfileController()).build();
+    }
+
+    @Test
+    public void checkRegisterView() throws Exception{
+        this.mockMvc.perform(get("/User/profile").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+        .andExpect(status().isOk())
+                .andExpect(content().contentType("applicatin/json"));
+    }
+}
