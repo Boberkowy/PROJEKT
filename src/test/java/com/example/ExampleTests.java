97d404612ecc1811028425d8934354aa4121d9e3
Karola <s12592@pjwstk.edu.pl>
2017-01-26 01:58:09 +0100

    próba testu

--- src/test/java/com/example/ExampleTests.java
+++ src/test/java/com/example/ExampleTests.java
@@ -1,29 +1,39 @@
 package com.example;
 
-import com.example.Controller.ProfileController;
+import com.example.Controller.RegisterController;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ExampleTests {
     private MockMvc mockMvc;
 
     @Before
     public void setup() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(new ProfileController()).build();
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new RegisterController()).build();
     }
 
     @Test
     public void checkRegisterView() throws Exception{
-        this.mockMvc.perform(post("/User/profile").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+        this.mockMvc.perform(post("/User/register")
                 .param("username", "uzytkownik")
-                .param
-        .andExpect(status().isOk())
-                .andExpect(content().contentType("applicatin/json"));
+                .param("password", "haslo")
+                .param("email", "email@wp.pl"))
+                .andExpect(status().isOk());
+             //   .andExpect(forwardedUrl("/User/profile"));
     }
 }
